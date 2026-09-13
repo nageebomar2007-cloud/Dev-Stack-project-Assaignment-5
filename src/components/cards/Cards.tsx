e0de2708f39c426858d9a4b63a72d3c115fae973
nageebomar2007-cloud <nageebomar2007@gmail.com>
2026-09-13 21:28:27 +0600

    cards section completed

--- src/components/cards/Cards.tsx
+++ src/components/cards/Cards.tsx
@@ -1,20 +1,20 @@
-// import { use } from "react";
-// import type { Icard } from "../../type/cardTypes";
-// import AllCards from "./AllCards";
+import { use } from "react";
+import type { Icard } from "../../type/cardTypes";
+import AllCards from "./AllCards";
 
-// interface CardProps {
-//   cardsPromise: Promise<Icard[]>;
-// }
-// const Cards = ({ cardsPromise }: CardProps) => {
-//   const cards = use(cardsPromise);
+interface CardProps {
+  cardsPromise: Promise<Icard[]>;
+}
+const Cards = ({ cardsPromise }: CardProps) => {
+  const cards = use(cardsPromise);
 
-//   return (
-//     <div>
+  return (
+    <div>
 
 
-//       <AllCards cards={cards} />
-//     </div>
-//   );
-// };
+      <AllCards cards={cards} />
+    </div>
+  );
+};
 
-// export default Cards;
+export default Cards;
